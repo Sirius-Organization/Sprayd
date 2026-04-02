251b7ff2f065221b18b50964affd5f44eeb2e7ba
lzkgmr <lzkgmr@gmail.com>
2026-04-02 21:02:31 +0300

    Add basic photo screen

--- Sprayd/Screens/ArtObjectScreen/ArtObjectView.swift
+++ Sprayd/Screens/ArtObjectScreen/ArtObjectView.swift
@@ -8,26 +8,39 @@
 import SwiftUI
 
 struct ArtObjectView: View {
+    @State private var viewModel = ArtObjectViewModel()
+
     var body: some View {
-        ZStack {
-            Color.appBackground
-                .ignoresSafeArea(edges: .all)
-            ScrollView {
-                Spacer(minLength: 20)
-                PhotoPagerView()
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Name of Art")
-                        .font(Font.InstrumentBold22)
-                    Text("73 is the 21-st prime number. Its mirror, 37, is the 12-th, and its mirror, 21, " +
-                        "is the product of multiplying, hang on to your hats, 7 and 3."
+        @Bindable var viewModel = viewModel
+
+        NavigationStack {
+            ZStack {
+                Color.appBackground
+                    .ignoresSafeArea(edges: .all)
+                ScrollView {
+                    Spacer(minLength: 20)
+                    PhotoPagerView(
+                        selectedPhotoIndex: $viewModel.selectedPhotoIndex,
+                        onPhotoTap: viewModel.openPhotoPreview
                     )
-                        .foregroundStyle(Color(.secondaryLabel))
-                        .font(Font.InstrumentMedium16)
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Name of Art")
+                            .font(Font.InstrumentBold22)
+                        Text(
+                            "73 is the 21-st prime number. Its mirror, 37, is the 12-th, and its mirror, 21, " +
+                            "is the product of multiplying, hang on to your hats, 7 and 3."
+                        )
+                            .foregroundStyle(Color(.secondaryLabel))
+                            .font(Font.InstrumentMedium16)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 20)
+                    .padding(.top, 8)
+                    .padding(.bottom, 20)
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal, 20)
-                .padding(.top, 8)
-                .padding(.bottom, 20)
+            }
+            .navigationDestination(isPresented: $viewModel.isPhotoPreviewPresented) {
+                ArtObjectPhotoPreviewView(selectedPhotoIndex: $viewModel.selectedPhotoIndex)
             }
         }
     }
