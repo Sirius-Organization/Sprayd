afc0e9ee909946b2234304afea985fe43d504188
loxxych <kate.coconutt@gmail.com>
2026-04-02 23:46:11 +0300

    separate font and color extensions

---
+++ Sprayd/Screens/ArtAdditionScreen/ArtAdditionView.swift
@@ -0,0 +1,7 @@
+//
+//  ArtAdditionView.swift
+//  Sprayd
+//
+//  Created by loxxy on 02.04.2026.
+//
+
---
+++ Sprayd/Utility/FontExtension.swift
@@ -0,0 +1,13 @@
+//
+//  FontExtension.swift
+//  Sprayd
+//
+//  Created by loxxy on 02.04.2026.
+//
+
+import SwiftUI
+
+extension Font {
+    static let InstrumentBold22 = Font.custom("InstrumentSans-Bold", size: 22)
+    static let InstrumentMedium16 = Font.custom("InstrumentSans-Medium", size: 16)
+}
